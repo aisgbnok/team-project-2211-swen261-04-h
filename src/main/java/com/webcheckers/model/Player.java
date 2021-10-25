b3bff54897415dc837b716c28cb052a80b62cb1f
Anthony Swierkosz <ajs2576@rit.edu>
2021-10-25 09:13:56 -0400

    PostSignInRoute & Player Code Cleanup - Improved code formatting and efficiency - Added improved sign in error messages - Added code documentation

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -2,33 +2,55 @@ package com.webcheckers.model;
 
 import java.util.Objects;
 
+/**
+ * The Player model.
+ *
+ * @author <a href='mailto:jwd2488@rit.edu'>Jake Downie</a>
+ * @author <a href='mailto:ajs2576@rit.edu'>Anthony Swierkosz</a>
+ */
 public class Player {
-    private String type;
 
-    public String getName() {
-        return name;
-    }
+  private String name;
+  // private String type;
 
-    public Player(String name) {
-        this.name = name;
-    }
+  /**
+   * Player Constructor
+   *
+   * @param name Player username
+   */
+  public Player(String name) {
+    this.name = name;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Player)) return false;
-        Player player = (Player) o;
-        return Objects.equals(getName(), player.getName());
-    }
+  // TODO: Should the player name be mutable? Any reason to allow it to change?
+  /**
+   * Sets the player username
+   *
+   * @param name Player username
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName());
-    }
+  /**
+   * Gets the player username
+   *
+   * @return Player username
+   */
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Player)) return false;
+    Player player = (Player) o;
+    return Objects.equals(getName(), player.getName());
+  }
 
-    private String name;
+  @Override
+  public int hashCode() {
+    return Objects.hash(getName());
+  }
 }
--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -12,81 +12,82 @@ import java.util.Objects;
 import java.util.logging.Logger;
 
 import static spark.Spark.halt;
+import static com.webcheckers.ui.GetHomeRoute.PLAYER_KEY;
 
 /**
- * The UI Controller to GET the Login page.
+ * The UI Controller to POST the user sign in.
  *
- * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
+ * @author <a href='mailto:jwd2488@rit.edu'>Jake Downie</a>
+ * @author <a href='mailto:ajs2576@rit.edu'>Anthony Swierkosz</a>
  */
 public class PostSignInRoute implements Route {
+  // Console Logger
   private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
 
-  private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
-    private static final Message USER_TAKEN = Message.info("Username taken, please select another.");
-  static final String PLAYER_KEY = "playerServices";
-  static final String TIMEOUT_SESSION_KEY = "timeoutWatchdog";
+  // Username Error Messages
+  private static final Message USER_EMPTY = Message.info("Username is empty!");
+  private static final Message USER_INVALID = Message.info("Username invalid, don't use quotes!");
+  private static final Message USER_TAKEN = Message.info("Username taken, please select another!");
 
   // The length of the session timeout in seconds
   static final int SESSION_TIMEOUT_PERIOD = 120;
+  static final String TIMEOUT_SESSION_KEY = "timeoutWatchdog";
+
+  // TemplateEngine used for HTML page rendering
   private final TemplateEngine templateEngine;
 
   /**
-   * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+   * Create the Spark Route (UI controller) to handle all {@code POST /} HTTP requests.
    *
-   * @param templateEngine
-   *   the HTML template rendering engine
+   * @param templateEngine the HTML template rendering engine
    */
   public PostSignInRoute(final TemplateEngine templateEngine) {
     this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
-    //
-    LOG.config("GetSignInRoute is initialized.");
+
+    LOG.config("PostSignInRoute is initialized.");
   }
 
   /**
-   * Render the WebCheckers Home page.
+   * Render the WebCheckers Sign In page.
    *
-   * @param request
-   *   the HTTP request
-   * @param response
-   *   the HTTP response
-   *
-   * @return
-   *   the rendered HTML for the Home page
+   * @param request the HTTP request
+   * @param response the HTTP response
+   * @return the rendered HTML for the Sign-In page
    */
   @Override
   public Object handle(Request request, Response response) {
 
     final Session httpSession = request.session();
-
-    LOG.finer("GetSignInRoute is invoked.");
-    //
+    LOG.finer("PostSignInRoute is invoked.");
     Map<String, Object> vm = new HashMap<>();
 
-    String param = request.queryParams("playerName");
-    param = param.strip();
-    if (param.length() == 0) {
-        response.redirect(WebServer.LOGIN_URL);
-        halt();
-        return null;
-    }
-    if (PlayerLobby.addPlayer(new Player(param))){
-        // get the object that will provide client-specific services for this player
-        final Player playerService = PlayerLobby.getPlayer(param);
-        httpSession.attribute(PLAYER_KEY, playerService);
+    // Get username
+    String username = request.queryParams("playerName").strip();
 
-        // setup session timeout. The valueUnbound() method in the SessionTimeoutWatchdog will
-        // be called when the session is invalidated. The next invocation of this route will
-        // have a new Session object with no attributes.
-        httpSession.attribute(TIMEOUT_SESSION_KEY, new SessionTimeoutWatchdog(playerService));
-        httpSession.maxInactiveInterval(SESSION_TIMEOUT_PERIOD);
-        response.redirect(WebServer.HOME_URL);
-        halt();
-        return null;
-
+    // Username is empty tell the user it is empty.
+    if (username.isEmpty()) {
+      vm.put("message", USER_EMPTY);
+      return templateEngine.render(new ModelAndView(vm, "signin.ftl"));
     }
-    else {
-        vm.put("message", USER_TAKEN);
-    return templateEngine.render(new ModelAndView(vm, "signin.ftl"));
+
+    // If the username is accepted
+    else if (PlayerLobby.addPlayer(new Player(username))) {
+      // Get the object that will provide client-specific services for this player
+      final Player playerService = PlayerLobby.getPlayer(username);
+      httpSession.attribute(PLAYER_KEY, playerService);
+
+      // setup session timeout. The valueUnbound() method in the SessionTimeoutWatchdog will
+      // be called when the session is invalidated. The next invocation of this route will
+      // have a new Session object with no attributes.
+      httpSession.attribute(TIMEOUT_SESSION_KEY, new SessionTimeoutWatchdog(playerService));
+      httpSession.maxInactiveInterval(SESSION_TIMEOUT_PERIOD);
+      response.redirect(WebServer.HOME_URL);
+      halt();
+      return null;
+
+    } else {
+      vm.put("message", USER_INVALID);
+      return templateEngine.render(new ModelAndView(vm, "signin.ftl"));
     }
   }
 }
