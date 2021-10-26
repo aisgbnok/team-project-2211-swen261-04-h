55a1fb07a40289564d1314b333ad030c7e53eb7e
Anthony Swierkosz <ajs2576@rit.edu>
2021-10-26 02:33:44 -0400

    Started setting up the Game class model

---
+++ src/main/java/com/webcheckers/model/Game.java
@@ -0,0 +1,42 @@
+package com.webcheckers.model;
+
+import java.util.Map;
+
+public class Game {
+  enum viewModes {
+    PLAY,
+    SPECTATOR,
+    REPLAY
+  }
+  enum colors {
+    RED, WHITE
+  }
+
+  private Player currentUser;
+  private viewModes viewMode;
+  private Map<String, Object> modeOptionsAsJSON;
+  private Player redPlayer;
+  private Player whitePlayer;
+  private colors activeColor;
+  private BoardView board;
+  private Message message;
+
+  public Game(Player redPlayer, Player whitePlayer) {
+    // Create Board
+    this.board =  new BoardView();
+
+    // Set Players
+    this.redPlayer = redPlayer;
+    this.whitePlayer = whitePlayer;
+
+    // RED is always the starting Player and color
+    this.currentUser = redPlayer;
+    this.activeColor = colors.RED;
+
+    // For all the MVP stories this variable must be set to PLAY.
+    this.viewMode = viewModes.PLAY;
+
+  }
+
+
+}
