1b68ab898d07df5ef32857637a8a2e88ca0b3c42
Anthony Swierkosz <ajs2576@rit.edu>
2021-10-26 02:15:47 -0400

    Player model code cleanup - Improved code documentation - Removed Player setName method, because player name does not need to be mutable.

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -3,44 +3,39 @@ package com.webcheckers.model;
 import java.util.Objects;
 
 /**
- * The Player model.
+ * The Player model used for instantiating players and holding their data.
  *
  * @author <a href='mailto:jwd2488@rit.edu'>Jake Downie</a>
  * @author <a href='mailto:ajs2576@rit.edu'>Anthony Swierkosz</a>
  */
 public class Player {
 
-  private String name;
-  // private String type;
+  private final String name;
 
   /**
-   * Player Constructor
+   * Player constructor sets the player name.
    *
-   * @param name Player username
+   * @param name Player username.
    */
   public Player(String name) {
     this.name = name;
   }
 
-  // TODO: Should the player name be mutable? Any reason to allow it to change?
   /**
-   * Sets the player username
+   * Gets the player's name.
    *
-   * @param name Player username
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  /**
-   * Gets the player username
-   *
-   * @return Player username
+   * @return Player username.
    */
   public String getName() {
     return name;
   }
 
+  /**
+   * Determiens if the provided Player instance is equal to this Player instance.
+   *
+   * @param o Player to compare equality.
+   * @return True if the two Players are equal, or false if they are not equal.
+   */
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -49,6 +44,11 @@ public class Player {
     return Objects.equals(getName(), player.getName());
   }
 
+  /**
+   * Generates Player hashcode.
+   *
+   * @return Player hashcode.
+   */
   @Override
   public int hashCode() {
     return Objects.hash(getName());
