bce605d069bfabf6c0819aa655f846128efa49dd
Anthony Swierkosz <ajs2576@rit.edu>
2021-11-06 03:55:17 -0400

    Added code documentation for Piece model

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -2,6 +2,11 @@ package com.webcheckers.model;
 
 import com.webcheckers.model.Game.Color;
 
+/**
+ * Piece on a space.
+ *
+ * @author <a href='mailto:ajs2576@rit.edu'>Anthony Swierkosz</a>
+ */
 public class Piece {
 
   // Enumerations
@@ -14,15 +19,31 @@ public class Piece {
   private final Type type;
   private final Color color;
 
+  /**
+   * Creates a board piece. Sets the type to the given type and the color to the given color.
+   *
+   * @param type Piece type enum, SINGLE or KING.
+   * @param color Piece color enum, WHITE or RED.
+   */
   protected Piece(Type type, Color color) {
     this.type = type;
     this.color = color;
   }
 
+  /**
+   * Getter for the piece type. SINGLE or KING.
+   *
+   * @return Type enum SINGLE or KING.
+   */
   public Type getType() {
     return type;
   }
 
+  /**
+   * Getter for the piece color. WHITE or RED.
+   *
+   * @return Color enum WHITE or RED.
+   */
   public Color getColor() {
     return color;
   }
