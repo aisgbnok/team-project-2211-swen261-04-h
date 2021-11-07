618156e0006d339f78dc546704357d56c6c485ed
Anthony Swierkosz <ajs2576@rit.edu>
2021-11-07 00:21:18 -0400

    Formatted PostResignRoute, PostSubmitTurnRoute, and WebServer

--- src/main/java/com/webcheckers/ui/PostResignRoute.java
+++ src/main/java/com/webcheckers/ui/PostResignRoute.java
@@ -20,35 +20,32 @@ import static com.webcheckers.ui.GetHomeRoute.CURRENT_PLAYER_KEY;
  * @author <a href='mailto:jwd2488@rit.edu'>Jake Downie</a>
  */
 public class PostResignRoute implements Route {
-    private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
+  private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
 
+  // TODO improve documentation
 
-    // TODO improve documentation
+  public PostResignRoute() {}
 
+  // TODO improve documentation
 
-    public PostResignRoute() {
-    }
+  /**
+   * Resign and redirect to the WebCheckers Home page.
+   *
+   * @param request the HTTP request
+   * @param response the HTTP response
+   * @return the rendered HTML for the Home page
+   */
+  @Override
+  public Object handle(Request request, Response response) {
+    LOG.finer("PostResignRoute is invoked.");
 
-    // TODO improve documentation
+    final Session httpSession = request.session();
+    Player player = httpSession.attribute(CURRENT_PLAYER_KEY);
 
-    /**
-     * Resign and redirect to the WebCheckers Home page.
-     *
-     * @param request  the HTTP request
-     * @param response the HTTP response
-     * @return the rendered HTML for the Home page
-     */
-    @Override
-    public Object handle(Request request, Response response) {
-        LOG.finer("PostResignRoute is invoked.");
-
-        final Session httpSession = request.session();
-        Player player = httpSession.attribute(CURRENT_PLAYER_KEY);
-
-        // TODO actually implement player resignation
-        // For now player is signed out
-       // PlayerLobby.removePlayer(player);
-       // httpSession.removeAttribute(CURRENT_PLAYER);
+    // TODO actually implement player resignation
+    // For now player is signed out
+    // PlayerLobby.removePlayer(player);
+    // httpSession.removeAttribute(CURRENT_PLAYER);
 
     // player.inGame(false);
 
--- src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
@@ -14,28 +14,27 @@ import java.util.logging.Logger;
  * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
  */
 public class PostSubmitTurnRoute implements Route {
-    private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
+  private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
 
+  /**
+   * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+   *
+   * @param templateEngine the HTML template rendering engine
+   */
+  public void PostSignInRoute(final TemplateEngine templateEngine) {
+    //
+    LOG.config("GetSignInRoute is initialized.");
+  }
 
-    /**
-     * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
-     *
-     * @param templateEngine the HTML template rendering engine
-     */
-    public void PostSignInRoute(final TemplateEngine templateEngine) {
-        //
-        LOG.config("GetSignInRoute is initialized.");
-    }
-
-    /**
-     * Render the WebCheckers Home page.
-     *
-     * @param request  the HTTP request
-     * @param response the HTTP response
-     * @return the rendered HTML for the Home page
-     */
-    @Override
-    public Object handle(Request request, Response response) {
+  /**
+   * Render the WebCheckers Home page.
+   *
+   * @param request the HTTP request
+   * @param response the HTTP response
+   * @return the rendered HTML for the Home page
+   */
+  @Override
+  public Object handle(Request request, Response response) {
 
     // final Session httpSession = request.session();
 
