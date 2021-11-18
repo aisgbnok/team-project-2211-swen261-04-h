d6a2cfccd827f06e66b86e64f0f0c4e2a169e408
jwd2488 <jwd2488@rit.edu>
2021-11-18 00:16:23 -0500

    starting submit turn
    (cherry picked from commit b585cd675546aa3d9c55e47a07cf22e9189aa182)

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -328,6 +328,9 @@ public class Board implements Iterable<Row> {
     return false;
   }
 
+  public ArrayList<Row> getRows(){
+    return rows;
+  }
   @Override
   public Iterator<Row> iterator() {
     return new Board(this).rows.iterator();
