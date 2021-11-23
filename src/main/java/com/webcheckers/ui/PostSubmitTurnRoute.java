915ec390676012d23fd13008ccc51c559d941894
Anthony Swierkosz <ajs2576@rit.edu>
2021-11-23 02:14:03 -0500

    Rewrote PostSubmitTurnRoute

--- src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
@@ -1,46 +1,55 @@
 package com.webcheckers.ui;
 
+import static com.webcheckers.ui.GetHomeRoute.CURRENT_PLAYER_KEY;
 
+import com.google.gson.Gson;
 import com.webcheckers.application.GameCenter;
-import com.webcheckers.model.Board;
 import com.webcheckers.model.Game;
-import com.webcheckers.util.Message;
 import com.webcheckers.model.Player;
-import spark.*;
-
+import java.util.UUID;
 import java.util.logging.Logger;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
 /**
- * The UI Controller to GET the Login page.
+ * UI Controller that handles turn submission. {@code POST /submitTurn}.
  *
- * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
+ * @author <a href='mailto:ajs2576@rit.edu'>Anthony Swierkosz</a>
+ * @author <a href='mailto:jwd2488@rit.edu'>Jake Downie</a>
  */
 public class PostSubmitTurnRoute implements Route {
-  private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
+  private static final Logger LOG = Logger.getLogger(PostSubmitTurnRoute.class.getName());
+
   /**
-   * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
-   *
-   * @param templateEngine the HTML template rendering engine
+   * Create the Spark Route (UI controller) to handle all {@code POST /submitTurn} HTTP requests.
    */
-  public void PostSignInRoute(final TemplateEngine templateEngine) {
-    //
-    LOG.config("GetSignInRoute is initialized.");
+  public PostSubmitTurnRoute() {
+    LOG.config("PostSubmitTurnRoute is initialized.");
   }
 
   /**
-   * Render the WebCheckers Home page.
+   * Handle turn submission and return appropriate json ajax response.
    *
-   * @param request the HTTP request
-   * @param response the HTTP response
-   * @return the rendered HTML for the Home page
+   * @param request The HTTP request
+   * @param response The HTTP response
+   * @return Json Ajax response
    */
   @Override
   public Object handle(Request request, Response response) {
+    LOG.finer("PostSubmitTurnRoute is invoked.");
 
-    Player sessionPlayer = request.session().attribute("Player");
-    Game game = GameCenter.getGame(sessionPlayer);
-    assert game != null;
-    return game.submitTurn(sessionPlayer).toString();
+    // Setup new Gson
+    Gson gson = new Gson();
 
+    // currentPlayer that wants to submit turn
+    Player player = request.session().attribute(CURRENT_PLAYER_KEY);
+
+    // Get the current game
+    UUID gameID = gson.fromJson(request.queryParams("gameID"), UUID.class);
+    Game game = GameCenter.getGame(gameID);
+
+    // Call submitTurn and return the result
+    return gson.toJson(game.submitTurn(player));
   }
 }
