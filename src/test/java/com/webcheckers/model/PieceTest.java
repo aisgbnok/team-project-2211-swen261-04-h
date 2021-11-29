016120091cd10b2045a4a0baf26933bb5376c622
Anthony Swierkosz <ajs2576@rit.edu>
2021-11-28 19:59:12 -0500

    Improved PieceTest documentation and added display names

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -1,12 +1,30 @@
 package com.webcheckers.model;
 
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.webcheckers.model.Piece.Type;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+
+/**
+ * The unit test suite for the {@link Piece} component.
+ *
+ * @author <a href='mailto:idc7947@rit.edu'>Ian Chasse</a>
+ * @author <a href='mailto:ajs2576@rit.edu'>Anthony Swierkosz</a>
+ */
 @Tag("Model-tier")
+@DisplayName("Piece Model Tests")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PieceTest {
+
     @Test
+    @Order(1)
+    @DisplayName("Piece Constructors")
     void testNewPiece(){
         Piece piece = new Piece(Piece.Type.SINGLE, Color.WHITE);
         assertNotNull(piece);
@@ -28,6 +46,8 @@ public class PieceTest {
     }
 
     @Test
+    @Order(4)
+    @DisplayName("Pieces can be Kinged")
     void testKing(){
         Piece piece = new Piece(Piece.Type.SINGLE, Color.WHITE);
         assertEquals(Piece.Type.SINGLE, piece.getType());
