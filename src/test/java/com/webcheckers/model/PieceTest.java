f83f03cd009078b73657d5669d20e34033dde6b9
Anthony Swierkosz <ajs2576@rit.edu>
2021-11-28 19:59:58 -0500

    Added PieceTest equal tests

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -56,4 +56,45 @@ public class PieceTest {
         assertEquals(Piece.Type.KING, piece.getType());
         assertNotEquals(Piece.Type.SINGLE, piece.getType());
     }
+
+  @Test
+  @Order(4)
+  @DisplayName("Similar Pieces are Equal")
+  void similarPiecesShouldBeEqual() {
+    // These pieces have the same properties and fields, but their references are not the same.
+    final Piece whiteSinglePiece1 = new Piece(Type.SINGLE, Color.WHITE);
+    final Piece whiteSinglePiece2 = new Piece(Type.SINGLE, Color.WHITE);
+
+    // Two "new" pieces with the same. They have the same fields but are not the "same"
+    assertEquals(whiteSinglePiece2, whiteSinglePiece1);
+
+    // Duplicating the pieces using Piece duplication constructor
+    final Piece duplicatePiece1 = new Piece(whiteSinglePiece1);
+    final Piece duplicatePiece2 = new Piece(whiteSinglePiece2);
+
+    // whiteSinglePiece1 and duplicatePiece1 are not the same object reference
+    assertEquals(whiteSinglePiece1, duplicatePiece1);
+    // whiteSinglePiece2 and duplicatePiece2 are not the same object reference
+    assertEquals(whiteSinglePiece2, duplicatePiece2);
+  }
+
+  @Test
+  @Order(5)
+  @DisplayName("Different type Pieces are not Equal")
+  void piecesOfDifferentTypeShouldNotBeEqual() {
+    // Create white single piece, duplicate it, and king duplicate
+    final Piece whiteSinglePiece = new Piece(Type.SINGLE, Color.WHITE);
+    final Piece whiteKingPiece = new Piece(whiteSinglePiece);
+    whiteKingPiece.king();
+
+    // Should not be equals
+    assertNotEquals(whiteSinglePiece, whiteKingPiece);
+
+    // Create red single piece and red king piece
+    final Piece redSinglePiece = new Piece(Type.SINGLE, Color.RED);
+    final Piece redKingPiece = new Piece(Type.KING, Color.RED);
+
+    // Should not be equals
+    assertNotEquals(redSinglePiece, redKingPiece);
+  }
 }
