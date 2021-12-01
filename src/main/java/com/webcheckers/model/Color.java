95dc8a89859bea58411e77f2eac5cd5021c66541
Anthony Swierkosz <ajs2576@rit.edu>
2021-12-01 16:05:41 -0500

    Added JavaDoc for Color.toString

--- src/main/java/com/webcheckers/model/Color.java
+++ src/main/java/com/webcheckers/model/Color.java
@@ -56,10 +56,18 @@ public enum Color {
     return direction().value();
   }
 
- @Override
+  /**
+   * Returns the friendly name for the enum. RED is Red, and WHITE is White.
+   *
+   * @return Friendly enum string
+   */
+  @Override
   public String toString() {
+    // Get original toString, and lowercase every character but the first
     String name = super.toString();
-    name = name.charAt(0) + name.substring(1).toLowerCase();
+    name = name.charAt(0) + name.substring(1).toLowerCase(); // "RED" -> "Red"
+
+    // Return friendly string
     return name;
   }
 
